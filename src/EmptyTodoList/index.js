ff3eed64d1a4ab87cbf232ee7efbd3f7a3f9fc4d
AndriwRC <randriw4@gmail.com>
2024-07-04 12:10:50 -0500

    set design for the empty todo list

--- src/EmptyTodoList/index.js
+++ src/EmptyTodoList/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { ReactComponent as CheckOutline } from '../TodoIcon/svg/check-outline.svg';
+import './EmptyTodoList.css';
 
 function EmptyTodoList() {
-  return <p>No hay todos!</p>;
+  return (
+    <>
+      <h3 className="empty-list__title">
+        ¡Genial! Ya no tienes tareas pendientes.
+      </h3>
+      <CheckOutline className="empty-list__icon" />
+    </>
+  );
 }
 
 export { EmptyTodoList };
