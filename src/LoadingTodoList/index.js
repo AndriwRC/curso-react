c0270ffd907a337f2458e31aad7c6869b9be6836
AndriwRC <randriw4@gmail.com>
2024-07-03 22:39:18 -0500

    create loading skeleton to simulate loading todo items

--- src/LoadingTodoList/index.js
+++ src/LoadingTodoList/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import './LoadingTodoList.css';
 
 function LoadingTodoList() {
-  return <p>Cargando...</p>;
+  return (
+    <>
+      <li className="todo-item todo-item--loading animate__beating-neutral">
+        <span className="item-loading__icon animate__beating-neutral"></span>
+        <p className="item__text item__text--loading animate__beating-neutral"></p>
+        <span className="item-loading__icon animate__beating-neutral"></span>
+      </li>
+      <li className="todo-item todo-item--loading animate__beating-neutral">
+        <span className="item-loading__icon animate__beating-neutral"></span>
+        <p className="item__text item__text--loading animate__beating-neutral"></p>
+        <span className="item-loading__icon animate__beating-neutral"></span>
+      </li>
+      <li className="todo-item todo-item--loading animate__beating-neutral">
+        <span className="item-loading__icon animate__beating-neutral"></span>
+        <p className="item__text item__text--loading animate__beating-neutral"></p>
+        <span className="item-loading__icon animate__beating-neutral"></span>
+      </li>
+    </>
+  );
 }
 
 export { LoadingTodoList };
